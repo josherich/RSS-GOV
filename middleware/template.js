b758fbce9ebf4e50bdc712ebd2845e001b4d756c
josherich <josherichchen@gmail.com>
2018-09-06 17:47:32 -0400

    feat(*): use new code from upstream

--- middleware/template.js
+++ middleware/template.js
@@ -1,43 +1,54 @@
 const art = require('art-template');
 const path = require('path');
 const config = require('../config');
+const typeRegrx = /\.(atom|rss|json)$/;
 
 module.exports = async (ctx, next) => {
+    ctx.state.type = ctx.request.path.match(typeRegrx) || ['', ''];
+    ctx.request.path = ctx.request.path.replace(typeRegrx, '');
+
     await next();
+
     if (!ctx.body) {
-        if (ctx.query.type === 'fragment') {
-            ctx.state.data.item = ctx.state.data.item.map(e => {
-                e.pubDate = new Date(e.pubDate).toLocaleDateString();
-                e.description = e.description ? e.description.replace('<br/>', '\n') : "";
-                return e;
-            })
-            ctx.set({
-                'Content-Type': 'text/html; charset=UTF-8',
-            });
-            ctx.body = art(path.resolve(__dirname, '../views/list.art'), {
-                lastBuildDate: new Date().toUTCString(),
-                ttl: config.cacheExpire,
-                ...ctx.state.data,
-            });
-        } else if (ctx.query.type === 'json' || ctx.url.includes('/chart/stats/')) {
-            ctx.body = JSON.stringify(ctx.state.data);
+        let template;
+
+        switch (ctx.state.type[1]) {
+            case 'atom':
+                template = path.resolve(__dirname, '../views/atom.art');
+                break;
+            case 'rss':
+                template = path.resolve(__dirname, '../views/rss.art');
+                break;
+            case 'json':
+                template = path.resolve(__dirname, '../views/json.art');
+                ctx.set({
+                    'Content-Type': 'application/json; charset=UTF-8',
+                });
+                break;
+            default:
+                template = path.resolve(__dirname, '../views/rss.art');
+                break;
+        }
+
+        if (ctx.url.includes('/chart/')) {
+            template = path.resolve(__dirname, '../views/chart.art');
+        }
+
+        if (ctx.url.includes('/chart/stats/')) {
+            template = path.resolve(__dirname, '../views/json.art');
             ctx.set({
                 'Content-Type': 'application/json; charset=UTF-8',
             });
-        } else {
-            if (ctx.url.includes('/chart/')) {
-                ctx.body = art(path.resolve(__dirname, '../views/chart.art'), {
-                    lastBuildDate: new Date().toUTCString(),
-                    ttl: config.cacheExpire,
-                    ...ctx.state.data,
-                });
-            } else {
-                ctx.body = art(path.resolve(__dirname, '../views/rss.art'), {
-                    lastBuildDate: new Date().toUTCString(),
-                    ttl: config.cacheExpire,
-                    ...ctx.state.data,
-                });
-            }
+        }
+
+        const data = {
+            lastBuildDate: new Date().toUTCString(),
+            updated: new Date().toISOString(),
+            ttl: config.cacheExpire,
+            ...ctx.state.data,
+        };
+        if (template) {
+            ctx.body = art(template, data);
         }
     }
 };
