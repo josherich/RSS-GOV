bd5a6ca72c1aa0a887e1a75be4d6b381cf022087
josherich <josherichchen@gmail.com>
2019-07-17 11:00:18 -0400

    update: gonganbu bulletin

--- routes/gonganbu/bulletin.js
+++ routes/gonganbu/bulletin.js
@@ -1,13 +1,13 @@
 const template = require('../template_page_chrome');
 
 const options = {
-    feed_title: '公安部 通知公告',
-    feed_desc: '公安部 通知公告',
+    feed_title: '公安部 要闻',
+    feed_desc: '公安部 要闻',
     feed_image: 'http://www.mps.gov.cn/template/3500247/3500300.gif',
-    feed_url: 'http://www.mps.gov.cn/n2254314/n2254457/n2254466/index.html',
-    url: 'http://www.mps.gov.cn/n2254314/n2254457/n2254466/index.html',
-    baseUrl: 'http://www.mps.gov.cn/n2254314/n2254457/n2254466/',
-    list_slr: ['dd', '#comp_3497341 dl'],
+    feed_url: 'http://www.mps.gov.cn/n2253534/n2253535/index.html',
+    url: 'http://www.mps.gov.cn/n2253534/n2253535/index.html',
+    baseUrl: 'http://www.mps.gov.cn/n2253534/n2253535/',
+    list_slr: ['dd', '#comp_5097045 dl'],
     title_slr: 'a',
     link_slr: 'a',
     link_rel: true,
